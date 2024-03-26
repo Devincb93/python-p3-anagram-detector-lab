5400ef1627c93b0150542e0a033785c5c296a314
Devin <devincb93@gmail.com>
2024-03-25 19:30:51 -0700

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
-# your code goes here!
+
+
+class Anagram: 
+    def __init__(self, word="listen"):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        result = []
+        sorted_word = sorted(self.word.lower()) 
+
+        for word in possible_anagrams:
+            sorted_word_candidate = sorted(word.lower())
+
+            if sorted_word == sorted_word_candidate and self.word.lower() != word.lower():
+                result.append(word)
+
+        return result
+    
+
+        
